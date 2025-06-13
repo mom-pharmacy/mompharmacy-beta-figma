a2413089dbab5fdfb01da58f893d025dedf81890
teja464 <tnukavarapu@gmail.com>
2025-06-13 18:36:52 +0530

    ok

---
+++ .history/utils/apiClient_20250613183422.tsx
@@ -0,0 +1,17 @@
+async function apiClient(path , options){
+    try{
+        const response = await fetch(`http://192.168.85.22:3000/${path}` , options);
+        console.log("this is res:",response)
+        if(response.ok){
+            const data = await response.json()
+            return data
+        }else{
+            return null
+        }
+    }catch(e){
+        console.log("Error in fetching data" , e)
+        return null
+    }
+}
+
+export default apiClient
